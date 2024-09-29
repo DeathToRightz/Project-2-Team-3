32c3c32cab2eca614843f5dd5cc218bba3f0de62
Angel Quintero <angelqv2014@gmail.com>
2024-09-28 20:10:07 -0400

    added fade in/out UI image

---
+++ Assets/Scripts/FadeTransition.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeTransition : MonoBehaviour
+{
+    private Image _image;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(SetAlpha(1));
+    }
+    
+    public void FadeOut()
+    {
+        StartCoroutine(SetAlpha(0));
+    }
+
+    private IEnumerator SetAlpha(float alphaValue)
+    {
+        alphaValue = Mathf.Clamp(alphaValue ,0, 1);
+        Color newColor = _image.color;
+
+        while (Math.Abs(newColor.a - alphaValue) > .0001f)
+        {
+            newColor.a = Mathf.MoveTowards(newColor.a, alphaValue, Time.deltaTime);
+            _image.color = newColor;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
