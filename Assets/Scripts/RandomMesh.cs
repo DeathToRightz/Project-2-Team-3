a58b74ad9ff11fd700e68e089d39d8ec57264724
Angel Quintero <angelqv2014@gmail.com>
2024-10-02 23:19:19 -0400

    Separated Gate parts and added opening animation.

--- Assets/Scripts/RandomMesh.cs
+++ Assets/Scripts/RandomMesh.cs
@@ -12,12 +12,5 @@ public class RandomMesh : MonoBehaviour
         GameObject newMesh = _meshes[Random.Range(0, _meshes.Length)];
         GetComponent<MeshFilter>().sharedMesh = newMesh.GetComponent<MeshFilter>().sharedMesh;
         GetComponent<Renderer>().sharedMaterials = newMesh.GetComponent<MeshRenderer>().sharedMaterials;
-        //Instantiate(newMesh, transform.position, transform.rotation, transform);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
